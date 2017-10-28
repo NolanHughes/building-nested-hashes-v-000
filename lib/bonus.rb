847d58b37f6f513ca4900affe8faf39248b53b83
Nolan Hughes <nhughes987@gmail.com>
2017-10-28 17:16:53 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,6 +19,7 @@ def bonus
       ]
    }
   }
+epic_tragedy[:montague][:hero][:status] = "dead"
 epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 epic_tragedy
@@ -29,5 +30,5 @@ epic_tragedy
 
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
-  
+
 end
