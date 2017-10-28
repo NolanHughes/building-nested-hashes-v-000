c1a2a4905c32f63d479ecc5219e6cf25310a7d11
Nolan Hughes <nhughes987@gmail.com>
2017-10-28 15:59:25 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,13 +6,15 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15"
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -27,5 +29,5 @@ def third_challenge
    }
   }
 
-  
+
 end
